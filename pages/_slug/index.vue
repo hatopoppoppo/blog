60f7508956bdbd51971cd7d5a2f7eb570745f4a2
hatohatopoppo <hatopoppo.web@gmail.com>
2022-12-17 22:58:05 +0900

    first commit

---
+++ pages/_slug/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="main">
+    <h1 class="title">{{ title }}</h1>
+    <p class="publishedAt">{{ publishedAt }}</p>
+    <div class="post" v-html="body"></div>
+  </main>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData({ params }) {
+    const { data } = await axios.get(
+      `https://hatopoppoblog.microcms.io/api/v1/blogs/${params.slug}`,
+      {
+        headers: { 'X-MICROCMS-API-KEY': process.env.MICROCMS_KEY }
+      }
+    )
+    return data
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  async asyncData() {
+    const { data } = await axios.get(
+      // your-service-id部分は自分のサービスidに置き換えてください
+      'https://hatopoppoblog.microcms.io/api/v1/blogs',
+      {
+        // your-api-key部分は自分のapi-keyに置き換えてください
+        headers: { 'X-MICROCMS-API-KEY': process.env.MICROCMS_KEY }
+      }
+    )
+    return data
+  }
+}
+</script>
