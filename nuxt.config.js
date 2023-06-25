b92d49d3318d69aa07a17e8e41665818ee9baa21
hatohatopoppo <hatopoppo.web@gmail.com>
2023-06-25 20:58:54 +0900

    description修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,14 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'blog',
+    title: 'ブログ',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'ja'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: '日記とかを書いたりする場所' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
